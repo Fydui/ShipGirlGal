d037e7882409ad46d83f48733ce19d68a678f7c7
Fydui <gtx980sli@yahoo.com>
2016-07-11 23:59:10 +0800

    好使

--- ShipGirlGAL/maincall.cpp
+++ ShipGirlGAL/maincall.cpp
@@ -1,9 +1,10 @@
 ﻿//-----本文件是对于maincall.h中部分函数的实现部分-----
 #include "sg_ui.h"
 #include "maincall.h"
-
+maincall* mainc;
+extern SG_UI* sgui;
 void maincall::StartGame()//入口函数
 {
-    SG_UI* sg;
-    sg->SG_MainUI();
+    mainc = this;
+    sgui->SG_MainUI();
 }
--- ShipGirlGAL/sg_ui.cpp
+++ ShipGirlGAL/sg_ui.cpp
@@ -1,14 +1,15 @@
 ﻿#include "sg_ui.h"
 #include "maincall.h"
-maincall* mainc;
-
+SG_UI* sgui;
+extern maincall* mainc;
 SG_UI::SG_UI()
 {
-
+//
 }
 
 
 void SG_UI::SG_MainUI()
 {
-
+    sgui = this;
+    mainc->AddPixmapItem("E:/Code/cpp/ShipGirlGAL/SG/Background/主界面背景.png",0,0);
 }
