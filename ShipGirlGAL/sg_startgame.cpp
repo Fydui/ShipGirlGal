45ffa38af998dec59739cf9c42e079fffea5aab5
Fydui <gtx980sli@yahoo.com>
2016-07-16 00:34:25 +0800

    开始动画

---
+++ ShipGirlGAL/sg_startgame.cpp
@@ -0,0 +1,23 @@
+﻿#include "sg_startgame.h"
+#include "sg_ui.h"
+SG_StartGame* ss;
+//extern SG_UI* sgui;
+
+SG_StartGame::SG_StartGame(library* s)
+{
+    sl = s;
+
+}
+
+void SG_StartGame::SG_StartUI()
+
+{
+    ss = this;
+    sl->AddPixmapItem(SG+"main_light_bg.png",0,0);
+
+    Item*up = sl->AddPixmapItem("E:/Code/cpp/ShipGirlGAL/SG/Background/strat_item_p.png",-1080,0);
+    sl->AnimationMoveItem(up,0,0,100);
+
+    Item*dow = sl->AddPixmapItem("E:/Code/cpp/ShipGirlGAL/SG/Background/start_item_d.png",1080,400);
+    sl->AnimationMoveItem(dow,-250,400,100);
+}
---
+++ ShipGirlGAL/sg_startgame.h
@@ -0,0 +1,15 @@
+﻿#ifndef SG_STARTGAME_H
+#define SG_STARTGAME_H
+#include "library.h"
+class SG_StartGame
+{
+public:
+    SG_StartGame(library* s);
+    void SG_StartUI();
+
+private:
+    library* sl;
+    const QString SG =  ":/SG/Background/";
+};
+
+#endif // SG_STARTGAME_H
